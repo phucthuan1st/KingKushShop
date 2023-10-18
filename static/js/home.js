135a84a71f83c3ee4039bb95cf5139cc2a0c7a1f
phucthuan1st <phucthuan1st@gmail.com>
2023-10-18 10:40:52 +0700

    use session storage for hiding popup

--- static/js/home.js
+++ static/js/home.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Check if the user has confirmed their age
-    const hasConfirmedAge = localStorage.getItem("confirmedAge");
+    const hasConfirmedAge = sessionStorage.getItem("confirmedAge");
 
     // If not, show the age verification popup
     if (!hasConfirmedAge) {
@@ -11,8 +11,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Handle user action when they press "Yes"
 document.getElementById("yesBtn").addEventListener("click", function() {
-    // Set a flag in local storage to indicate age confirmation
-    localStorage.setItem("confirmedAge", "true");
+    // Set a flag in sessionStorage to indicate age confirmation
+    sessionStorage.setItem("confirmedAge", "true");
 
     // Hide the age verification popup
     const ageVerificationPopup = document.getElementsByClassName("age-popup")[0];
